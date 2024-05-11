66578abe54305518517f90893d2d2e35cb16fcdf
hupeng <hup_dev@outlook.com>
2024-05-11 15:00:57 +0800

    Trim functionality explained

--- common-base/src/main/java/com/luban/common/base/annotation/Trim.java
+++ common-base/src/main/java/com/luban/common/base/annotation/Trim.java
@@ -9,7 +9,33 @@ import java.lang.annotation.*;
 /**
  * Based on {@code cn.hutool.core.util.StrUtil.trim(value, mode)}
  * <p>
+ * Usage:
+ * <p>
+ * Add this annotation on parameters that were also annotated with {@code @RequestBody} to activate the processing,
+ * and the fields in the parameters will be checked recursively.
+ * <p>
+ * To trim fields, annotated on the specific field.
+ * <p>
+ * If the field is not a String type, the fields to be trimmed in the object field also has to be annotated
+ * with {@code @Trim}.
  *
+ *
+ * <pre>
+ * public MODIFIER method({@code @RequestBody @Trim }ParamClass){}
+ *
+ * class ParamClass {
+ *      {@code @Trim}
+ *      private String trimmingField;
+ *
+ *      {@code @Trim}
+ *      private NoneStringField nonStringField;
+ * }
+ *
+ * class NoneStringField {
+ *      {@code @Trim}
+ *      private String nonStringTrimmingField;
+ * }
+ * </pre>
  *
  * @author hp
  * @see com.luban.common.base.http.servlet.TrimRequestResponseBodyMethodProcessorDecorator
