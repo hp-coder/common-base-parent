f0d8841d31d164dbc7d3096a468ec0fcd008c20d
hupeng <hup_dev@outlook.com>
2024-05-11 16:00:18 +0800

    Mybatis DDD design adapter
    This Hooks design allows clients to publish domain events as if they are using the JPA.

--- orm-common-base/src/main/java/com/luban/orm/api/AbstractOrmOperator.java
+++ orm-common-base/src/main/java/com/luban/orm/api/AbstractOrmOperator.java
@@ -1,11 +1,15 @@
 package com.luban.orm.api;
 
+import cn.hutool.core.collection.CollUtil;
+import com.google.common.collect.Lists;
 import com.luban.common.base.enums.CodeEnum;
 import com.luban.common.base.exception.BusinessException;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -17,12 +21,11 @@ import java.util.function.Supplier;
 @Slf4j
 public abstract class AbstractOrmOperator<AGGREGATE_ROOT, REPOSITORY extends OrmRepository<AGGREGATE_ROOT, ID>, ID extends Serializable> implements Modifier<AGGREGATE_ROOT>, Hooks<AGGREGATE_ROOT> {
     protected final REPOSITORY repository;
-
     protected AGGREGATE_ROOT aggregation;
-
-    protected Consumer<AGGREGATE_ROOT> onSuccess = entity -> log.info("Successfully Saved");
-
-    protected Consumer<? super Throwable> onFailure = e -> {
+    protected List<Consumer<AGGREGATE_ROOT>> onSuccessConsumers = Lists.newArrayList();
+    protected List<Consumer<? super Throwable>> onFailureConsumers = Lists.newArrayList();
+    protected Consumer<AGGREGATE_ROOT> onSuccessDefault = entity -> log.info("{} is successfully Saved", entity.getClass().getName());
+    protected Consumer<? super Throwable> onFailureDefault = e -> {
         throw new BusinessException(CodeEnum.SaveError, e);
     };
 
@@ -37,23 +40,31 @@ public abstract class AbstractOrmOperator<AGGREGATE_ROOT, REPOSITORY extends Orm
     }
 
     @Override
-    public Hooks<AGGREGATE_ROOT> onSuccess(Consumer<AGGREGATE_ROOT> consumer) {
-        this.onSuccess = Objects.requireNonNull(consumer);
+    public Hooks<AGGREGATE_ROOT> registerOnSuccess(Consumer<AGGREGATE_ROOT> consumer) {
+        this.onSuccessConsumers.addLast(Objects.requireNonNull(consumer));
         return this;
     }
 
     @Override
-    public Hooks<AGGREGATE_ROOT> onFailure(Consumer<? super Throwable> consumer) {
-        this.onFailure = Objects.requireNonNull(consumer);
+    public Hooks<AGGREGATE_ROOT> registerOnFailure(Consumer<? super Throwable> consumer) {
+        this.onFailureConsumers.addLast(Objects.requireNonNull(consumer));
         return this;
     }
 
     @Override
     public Optional<AGGREGATE_ROOT> execute() {
         return Optional.ofNullable(Try.of(() -> Objects.requireNonNull(doExecute()).get())
-                .onSuccess(this.onSuccess)
-                .onFailure(this.onFailure)
+                .onSuccess(aggregateRoot -> getOnSuccessConsumers().forEach(consumer -> consumer.accept(aggregateRoot)))
+                .onFailure(throwable -> getOnFailureConsumers().forEach(consumer -> consumer.accept(throwable)))
                 .getOrNull());
+    }
+
+    protected List<Consumer<AGGREGATE_ROOT>> getOnSuccessConsumers() {
+        return CollUtil.defaultIfEmpty(this.onSuccessConsumers, Collections.singletonList(this.onSuccessDefault));
+    }
+
+    public List<Consumer<? super Throwable>> getOnFailureConsumers() {
+        return CollUtil.defaultIfEmpty(this.onFailureConsumers, Collections.singletonList(this.onFailureDefault));
     }
 
     protected abstract Supplier<AGGREGATE_ROOT> doExecute();
--- orm-common-base/src/main/java/com/luban/orm/api/Hooks.java
+++ orm-common-base/src/main/java/com/luban/orm/api/Hooks.java
@@ -7,8 +7,8 @@ import java.util.function.Consumer;
  */
 public interface Hooks<AGGREGATE_ROOT> extends Executor<AGGREGATE_ROOT>{
 
-    Hooks<AGGREGATE_ROOT> onSuccess(Consumer<AGGREGATE_ROOT> consumer);
+    Hooks<AGGREGATE_ROOT> registerOnSuccess(Consumer<AGGREGATE_ROOT> consumer);
 
-    Hooks<AGGREGATE_ROOT> onFailure(Consumer<? super Throwable> consumer);
+    Hooks<AGGREGATE_ROOT> registerOnFailure(Consumer<? super Throwable> consumer);
 
 }
