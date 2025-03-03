5003f7778cc9cabdbf08fbd9c2462fd53f9f15d6
hupeng <hup_dev@outlook.com>
2025-03-03 18:37:03 +0800

    change codes

--- common-base/src/main/java/com/hp/common/base/model/Returns.java
+++ common-base/src/main/java/com/hp/common/base/model/Returns.java
@@ -24,24 +24,45 @@ public class Returns<T> {
     private T data;
 
     public static <T> Returns<T> of(BaseEnum<?, Integer> baseEnum) {
-        return new Returns<>(baseEnum);
+        return Returns.of(baseEnum, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Returns<T> of(BaseEnum<?, Integer> baseEnum, T data) {
+        final Returns<Object> objectReturns = new Returns<>(baseEnum);
+        return ((Returns<T>) objectReturns.data(data));
     }
 
     public static <T> Returns<T> success() {
         return Returns.of(DefaultCode.SUCCESS);
     }
 
-    @SuppressWarnings("unchecked")
     public static <T> Returns<T> success(T data) {
-        return (Returns<T>) Returns.success().data(data);
+        return Returns.of(DefaultCode.SUCCESS, data);
+    }
+
+    public static <T> Returns<T> success(BaseEnum<? extends Enum<?>, Integer> code) {
+        return Returns.of(code, null);
+    }
+
+    public static <T> Returns<T> success(BaseEnum<? extends Enum<?>, Integer> code, T data) {
+        return Returns.of(code, data);
     }
 
     public static <T> Returns<T> fail() {
-        return Returns.of(DefaultCode.FAIL);
+        return Returns.of(DefaultCode.FAIL, null);
     }
 
-    public static <T> Returns<T> fail(BaseEnum<? extends Enum<?>, Integer> codeEnum) {
-        return new Returns<>(codeEnum);
+    public static <T> Returns<T> fail(T data) {
+        return Returns.of(DefaultCode.FAIL, data);
+    }
+
+    public static <T> Returns<T> fail(BaseEnum<? extends Enum<?>, Integer> code) {
+        return Returns.of(code, null);
+    }
+
+    public static <T> Returns<T> fail(BaseEnum<? extends Enum<?>, Integer> code, T data) {
+        return Returns.of(code, data);
     }
 
     protected Returns() {
